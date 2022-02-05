a9e00bf273c24caa42fca02bd68fa6e1ed83e920
unknown <amine.bd@gmail.com>
2022-02-05 17:03:46 +0100

    add sports page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const sports = ["soccer", "basketball", "tennis", "baseball", "volleyball"];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('Hello form Express');
+});
+
+router.get('/sports', function(req, res, next) {
+  res.send(sports);
 });
 
 module.exports = router;
